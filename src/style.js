16712800a3d8fa441050348c7675b951a4ecba42
Treehole <thatxjtu@qq.com>
2019-02-27 19:15:21 +0800

    P53

---
+++ src/style.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 200;
+    padding: 0;
+    font-family: sans-serif;
+    background-color: green;
+  }
+`;
